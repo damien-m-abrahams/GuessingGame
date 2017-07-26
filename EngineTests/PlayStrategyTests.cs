5b3753c4d3614ec267ddc1542c0bdb4a102b7bc0
damienabrahams <damien.abrahams@insightsoftware.com>
2017-07-26 05:16:01 +0100

    Remove question loop from test. It can cause an infinite loop.

--- EngineTests/PlayStrategyTests.cs
+++ EngineTests/PlayStrategyTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Security.Permissions;
+﻿using System.Linq;
 using GuessingGame.Engine;
 using GuessingGame.Subjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -24,21 +22,17 @@ namespace EngineTests
 
             sut.SelectedSubject(possibleSubjects[1]);
 
-			var proceed = true;
-			do {
-				var question = sut.Question();
-				if (question != null) {
-					sut.Answer(false);
-				} else {
-					proceed = false;
-				}
-			} while (proceed);
-
-            // Answering a question as false is sometimes an incorrect response meaning the system can't determine an answer
+			var question = sut.Question();
+			// Answering a question as false is sometimes an incorrect response meaning the system can't determine an answer
 			// This test is needs to be expanded to answer yes to any questions pretaining to the selected subject
-            sut.Answer(false);
+			sut.Answer(false);
 
-            var answer = sut.CalculatedSubject();
+			question = sut.Question();
+	        if (question != null) {
+				sut.Answer(false);
+			}
+
+			var answer = sut.CalculatedSubject();
             Assert.IsTrue(answer.Text == string.Format(possibleSubjects[0].AnswerPattern, possibleSubjects[0].Name));
         }
 
@@ -54,17 +48,17 @@ namespace EngineTests
 
             sut.SelectedSubject(possibleSubjects[1]);
 
-	        var proceed = true;
-	        do {
-		        var question = sut.Question();
-		        if (question != null) {
-			        sut.Answer(true);
-		        } else {
-			        proceed = false;
-		        }
-	        } while (proceed);
-                       
-            var answer = sut.CalculatedSubject();
+			var question = sut.Question();
+			// Answering a question as false is sometimes an incorrect response meaning the system can't determine an answer
+			// This test is needs to be expanded to answer yes to any questions pretaining to the selected subject
+			sut.Answer(true);
+
+			question = sut.Question();
+	        if (question != null) {
+		        sut.Answer(true);
+	        }
+
+	        var answer = sut.CalculatedSubject();
             Assert.IsTrue(answer.Text == string.Format(possibleSubjects[1].AnswerPattern, possibleSubjects[1].Name));
         }
     }
